ce54d597731c1525b934dc7ff915fc6b6ba1626b
GonzaBianchi <gonzalobianchini70@gmail.com>
2025-06-22 15:07:35 -0300

    refactor: simplify MongoDB connection options

--- src/app.js
+++ src/app.js
@@ -31,10 +31,8 @@ const client = new Client({
 client.commands = new Collection();
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Dynamic event handler loader
